c4b3138cbfa6e08f26cc236e1132e56473e12454
.br <boukengrochinel15@gmail.com>
2025-08-28 13:51:15 +0100

    stuffs

--- core/use-cases/register-user.js
+++ core/use-cases/register-user.js
@@ -1,6 +1,6 @@
-const User = require("../entities/user.model");
+const { User } = require("../entities/user"); // Import your pure entity class
 const bcrypt = require("bcrypt");
-const SALT_ROUNDS = 10; // Standard for bcrypt hashing
+const SALT_ROUNDS = 10;
 
 class RegisterUser {
   constructor(userRepository, blockchainService) {
@@ -10,12 +10,10 @@ class RegisterUser {
 
   async execute(phoneNumber, name, password) {
     try {
-      // 1. Valider le mot de passe
       if (!password || password.length < 8) {
         throw new Error("Le mot de passe doit contenir au moins 8 caractères.");
       }
 
-      // 2. Vérifier si l'utilisateur existe déjà
       const existingUser = await this.userRepository.findByPhoneNumber(
         phoneNumber
       );
@@ -25,21 +23,20 @@ class RegisterUser {
         );
       }
 
-      // 3. Créer un wallet blockchain
+      // Create a blockchain wallet
       const wallet = await this.blockchainService.createWallet();
 
-      // 4. HASHER le mot de passe avant de le sauvegarder
+      // Hash the password
       const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
 
-      // 5. ENCRYPTER la clé privée avec le mot de passe
-      // This function should be in your blockchainService
+      // Encrypt the private key
       const encryptedPrivateKey =
         await this.blockchainService.encryptPrivateKey(
           wallet.privateKey,
           password
         );
 
-      // 6. Créer l'entité utilisateur avec les données sécurisées
+      // CORRECTION IS HERE: We create the pure entity object first.
       const user = User.create(
         phoneNumber,
         wallet.address,
@@ -48,21 +45,15 @@ class RegisterUser {
         encryptedPrivateKey
       );
 
-      // 7. Sauvegarder l'utilisateur
+      // Then, we pass this complete object to the repository to be saved.
       await this.userRepository.save(user);
 
-      // NE JAMAIS RETOURNER LA CLÉ PRIVÉE EN CLAIR
       return {
         success: true,
         user: {
           id: user.id,
           name: user.name,
           walletAddress: user.walletAddress,
-        },
-        // We return the encrypted key, which is safe to store and show
-        wallet: {
-          address: wallet.address,
-          encryptedPrivateKey: encryptedPrivateKey,
         },
       };
     } catch (error) {
